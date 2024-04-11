e4dd79253bea0ea8043ef6341f133f631159ee35
Trần Thế Khôi <tranthekhoi2003@gmail.com>
2024-04-11 21:21:17 +0700

    TTK DangNhap

--- PTUD_QuanLyKhachSan/src/giaodien/Login.java
+++ PTUD_QuanLyKhachSan/src/giaodien/Login.java
@@ -2,6 +2,11 @@
 package giaodien;
 import giaodien.CustomClass.ChangePassword;
 import java.awt.Color;
+import java.sql.Connection;
+
+import javax.swing.JOptionPane;
+
+import dao.TaiKhoanDao;
 public class Login extends javax.swing.JFrame {
 
     public Login() {
@@ -20,10 +25,10 @@ public class Login extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        textField1 = new giaodien.CustomClass.TextField();
-        passwordField1 = new giaodien.CustomClass.PasswordField();
-        button1 = new giaodien.CustomClass.Button();
-        button2 = new giaodien.CustomClass.Button();
+        txtTenTaiKhoan = new giaodien.CustomClass.TextField();
+        txtMatKhau = new giaodien.CustomClass.PasswordField();
+        btnDangNhap = new giaodien.CustomClass.Button();
+        btnDoiMatKhau = new giaodien.CustomClass.Button();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("LOGIN");
@@ -73,41 +78,56 @@ public class Login extends javax.swing.JFrame {
         jLabel6.setFont(new java.awt.Font("Showcard Gothic", 0, 18)); // NOI18N
         jLabel6.setText("StaRail Hotel  Management");
 
-        textField1.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
-        textField1.setLabelText("Tên đăng nhập");
-        textField1.setLineColor(new java.awt.Color(23, 195, 178));
-        textField1.setPreferredSize(new java.awt.Dimension(64, 50));
-
-        passwordField1.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
-        passwordField1.setLabelText("Mật khẩu");
-        passwordField1.setLineColor(new java.awt.Color(23, 195, 178));
-
-        button1.setBackground(new java.awt.Color(23, 195, 178));
-        button1.setBorder(null);
-        button1.setText("ĐĂNG NHẬP");
-        button1.setBorderColor(new java.awt.Color(23, 195, 178));
-        button1.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
-        button1.setRadius(20);
-        button1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                button1MouseExited(evt);
-            }
-        });
-
-        button2.setBackground(new java.awt.Color(254, 109, 115));
-        button2.setBorder(null);
-        button2.setText("Đổi mật khẩu");
-        button2.setBorderColor(new java.awt.Color(254, 109, 115));
-        button2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        button2.setRadius(15);
-        button2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                button2MouseExited(evt);
-            }
-        });
-        button2.addActionListener(new java.awt.event.ActionListener() {
+        txtTenTaiKhoan.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
+        txtTenTaiKhoan.setLabelText("Tên đăng nhập");
+        txtTenTaiKhoan.setLineColor(new java.awt.Color(23, 195, 178));
+        txtTenTaiKhoan.setPreferredSize(new java.awt.Dimension(64, 50));
+        txtTenTaiKhoan.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button2ActionPerformed(evt);
+                txtTenTaiKhoanActionPerformed(evt);
+            }
+        });
+
+        txtMatKhau.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
+        txtMatKhau.setLabelText("Mật khẩu");
+        txtMatKhau.setLineColor(new java.awt.Color(23, 195, 178));
+        txtMatKhau.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtMatKhauActionPerformed(evt);
+            }
+        });
+
+        btnDangNhap.setBackground(new java.awt.Color(23, 195, 178));
+        btnDangNhap.setBorder(null);
+        btnDangNhap.setText("ĐĂNG NHẬP");
+        btnDangNhap.setBorderColor(new java.awt.Color(23, 195, 178));
+        btnDangNhap.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
+        btnDangNhap.setRadius(20);
+        btnDangNhap.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnDangNhapMouseExited(evt);
+            }
+        });
+        btnDangNhap.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDangNhapActionPerformed(evt);
+            }
+        });
+
+        btnDoiMatKhau.setBackground(new java.awt.Color(254, 109, 115));
+        btnDoiMatKhau.setBorder(null);
+        btnDoiMatKhau.setText("Đổi mật khẩu");
+        btnDoiMatKhau.setBorderColor(new java.awt.Color(254, 109, 115));
+        btnDoiMatKhau.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnDoiMatKhau.setRadius(15);
+        btnDoiMatKhau.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnDoiMatKhauMouseExited(evt);
+            }
+        });
+        btnDoiMatKhau.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDoiMatKhauActionPerformed(evt);
             }
         });
 
@@ -121,7 +141,7 @@ public class Login extends javax.swing.JFrame {
                     .addGroup(LeftLayout.createSequentialGroup()
                         .addComponent(jLabel4)
                         .addGap(18, 18, 18)
-                        .addComponent(button2, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnDoiMatKhau, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap())
                     .addGroup(LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, LeftLayout.createSequentialGroup()
@@ -133,9 +153,9 @@ public class Login extends javax.swing.JFrame {
             .addGroup(LeftLayout.createSequentialGroup()
                 .addGap(76, 76, 76)
                 .addGroup(LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(textField1, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
-                    .addComponent(passwordField1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(button1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(txtTenTaiKhoan, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+                    .addComponent(txtMatKhau, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnDangNhap, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         LeftLayout.setVerticalGroup(
@@ -144,15 +164,15 @@ public class Login extends javax.swing.JFrame {
                 .addGap(64, 64, 64)
                 .addComponent(jLabel1)
                 .addGap(32, 32, 32)
-                .addComponent(textField1, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtTenTaiKhoan, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(32, 32, 32)
-                .addComponent(passwordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtMatKhau, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(28, 28, 28)
-                .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnDangNhap, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
                 .addGroup(LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(button2, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnDoiMatKhau, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(29, 29, 29)
                 .addComponent(jLabel6)
                 .addGap(42, 42, 42))
@@ -181,22 +201,49 @@ public class Login extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void button1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_button1MouseExited
-            button1.setBackground(new Color(23,195,178)); 
-    }//GEN-LAST:event_button1MouseExited
+    private void btnDangNhapMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnDangNhapMouseExited
+            btnDangNhap.setBackground(new Color(23,195,178)); 
+    }//GEN-LAST:event_btnDangNhapMouseExited
 
-    private void button2MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_button2MouseExited
-        button2.setBackground(new Color(254,109,115)); 
-    }//GEN-LAST:event_button2MouseExited
+    private void btnDoiMatKhauMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnDoiMatKhauMouseExited
+        btnDoiMatKhau.setBackground(new Color(254,109,115)); 
+    }//GEN-LAST:event_btnDoiMatKhauMouseExited
 
-    private void button2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button2ActionPerformed
+    private void btnDoiMatKhauActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDoiMatKhauActionPerformed
         // TODO add your handling code here:
         ChangePassword changePasswordFrame = new ChangePassword();
         changePasswordFrame.setVisible(true);
         changePasswordFrame.pack();
         changePasswordFrame.setLocationRelativeTo(null);
         this.dispose();
-    }//GEN-LAST:event_button2ActionPerformed
+    }//GEN-LAST:event_btnDoiMatKhauActionPerformed
+
+    private void txtMatKhauActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtMatKhauActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtMatKhauActionPerformed
+
+    private void txtTenTaiKhoanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTenTaiKhoanActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtTenTaiKhoanActionPerformed
+
+    private void btnDangNhapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDangNhapActionPerformed
+        String tenTaiKhoan = txtTenTaiKhoan.getText();
+        char[] matKhau = txtMatKhau.getPassword();
+        String matKhauString = new String(matKhau);
+        TaiKhoanDao taiKhoanDao = new TaiKhoanDao();
+        if (taiKhoanDao.timTaiKhoanTheoTenDangNhap(tenTaiKhoan) == null) {
+            JOptionPane.showMessageDialog(this, "Tên đăng nhập không tồn tại");
+        }
+//        if (taiKhoanDao.timTaiKhoanTheoTenDangNhap(tenTaiKhoan).getMatKhau().equals(matKhauString)) {
+//            JOptionPane.showMessageDialog(this, "Đăng nhập thành công");
+//            this.dispose();
+//            new Login().setVisible(true);
+//        }
+//        else {
+//            JOptionPane.showMessageDialog(this, "Sai mật khẩu");
+//        }
+        
+    }//GEN-LAST:event_btnDangNhapActionPerformed
 
     /**
      * @param args the command line arguments
@@ -205,14 +252,22 @@ public class Login extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Left;
     private javax.swing.JPanel Right;
-    private giaodien.CustomClass.Button button1;
-    private giaodien.CustomClass.Button button2;
+    private giaodien.CustomClass.Button btnDangNhap;
+    private giaodien.CustomClass.Button btnDoiMatKhau;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
-    private giaodien.CustomClass.PasswordField passwordField1;
-    private giaodien.CustomClass.TextField textField1;
+    private giaodien.CustomClass.PasswordField txtMatKhau;
+    private giaodien.CustomClass.TextField txtTenTaiKhoan;
     // End of variables declaration//GEN-END:variables
+    
+    public static void main(String[] args) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Login().setVisible(true);
+			}
+		});
+	}
 }
